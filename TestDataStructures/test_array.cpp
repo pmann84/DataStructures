00c4fa961a3475be7dd1f02c98a2cf87e9b27d34
pmann84 <peterdmann28@gmail.com>
2020-08-14 14:22:57 +0100

    Minor tweak to gtest setup to get a simple test working

--- TestDataStructures/test_array.cpp
+++ TestDataStructures/test_array.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 
-TEST(array, firsttest)
-{
+#include "array.h"
 
+TEST(array_tests, check_array_access_returns_correct_value)
+{
+   std::string test_value = "Hello";
+   ds::array<std::string, 5> a;
+   a[1] = test_value;
+   EXPECT_EQ(test_value, a[1]);
 }
